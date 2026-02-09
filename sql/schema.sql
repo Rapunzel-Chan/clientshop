d011fd7e314bfd7744acc1c645dcf713a697ea1b
RapunzelChan <rapuncel.chan24@gmail.com>
2026-02-09 10:55:17 +0300

    add BD schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,43 @@
+Category (неограниченная вложенность)
+
+| Поле      | Тип              | Описание               |
+| --------- | ---------------- | ---------------------- |
+| id        | PK               |                        |
+| name      | varchar          | Наименование категории |
+| parent_id | FK → category.id | Родительская категория |
+
+Product (номенклатура)
+
+| Поле        | Тип              |
+| ----------- | ---------------- |
+| id          | PK               |
+| name        | varchar          |
+| quantity    | int              |
+| price       | numeric          |
+| category_id | FK → category.id |
+
+Client
+
+| Поле    | Тип     |
+| ------- | ------- |
+| id      | PK      |
+| name    | varchar |
+| address | text    |
+
+Order_type
+
+| Поле       | Тип            |
+| ---------- | -------------- |
+| id         | PK             |
+| client_id  | FK → client.id |
+| created_at | timestamp      |
+
+Order_item (нормализация, заказ из разного набора товаров)
+
+| Поле            | Тип                  |
+| --------------- | ------------------   |
+| id              | PK                   |
+| order_type_id   | FK → order_type.id   |
+| product_id      | FK → product.id      |
+| quantity        | int                  |
+| price_at_moment | numeric              |
